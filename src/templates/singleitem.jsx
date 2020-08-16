2ecb4476c63a7c7335f99dba63689c075b065993
andrew@ecomloop <andrew@ecomloop.com>
2020-08-16 13:39:30 -0700

    update sitemap

--- src/templates/singleitem.jsx
+++ src/templates/singleitem.jsx
@@ -135,7 +135,7 @@ const SingleItem = ({ data, pageContext }) => {
         <Content input={fields.videoEmbedData} /><br />
         <Content input={fields.tweetEmbedData} /><br />
 
-        <a href="/random" className="button ">See another police brutality case </a>
+        <a href="/random/" className="button ">See another police brutality case </a>
         <a className="button buttonalt" href="javascript:void(0)" onClick={() => setOpen(true)}>
           Help end police Violence
         </a>
